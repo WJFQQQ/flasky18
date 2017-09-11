c421b3e5fb4f94f377c4d2835a52d628fbaaa684
wjf <597898719@qq.com>
2017-09-11 11:59:10 +0800

    add utf-8 in app/email and set a new value:charset

--- app/email.py
+++ app/email.py
@@ -4,6 +4,8 @@ from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
 
+from email import charset
+charset.add_charset('utf-8', charset.SHORTEST, charset.BASE64, 'utf-8')
 
 def send_async_email(app, msg):
     with app.app_context():
@@ -13,9 +15,9 @@ def send_async_email(app, msg):
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['MAIL_SENDER'], recipients=[to])
-    msg.body = render_template(template + '.txt', **kwargs).encode('utf-8')
-    msg.html = render_template(template + '.html', **kwargs).encode('utf-8')
+                  sender=app.config['MAIL_SENDER'], recipients=[to],charset='utf-8')##加入该关键字参数，使发送的html支持utf-8编码
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
