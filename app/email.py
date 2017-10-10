25bfaa04ecd5ddba979f4ab8740f5df31cbc50d3
WJFQQQ <31752368+WJFQQQ@users.noreply.github.com>
2017-10-10 12:07:37 +0800

    Update email.py

--- app/email.py
+++ app/email.py
@@ -15,7 +15,7 @@ def send_async_email(app, msg):
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['MAIL_SENDER'], recipients=[to],charset='utf-8')##加入该关键字参数，使发送的html支持utf-8编码
+                  sender=app.config['MAIL_SENDER'], recipients=[to],charset='utf-8')##加入 charset 该关键字参数，使发送的html支持utf-8编码
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
